f55064fb68c735c6857e624bd9d066e7eb886365
pmani-250 <78112386+pmani-250@users.noreply.github.com>
2025-03-23 08:05:04 -0700

    finish progress section in procedure

--- beachhacks_2025/src/app/progressBar/progressBar.jsx
+++ beachhacks_2025/src/app/progressBar/progressBar.jsx
@@ -1,25 +1,45 @@
-import React from "react";
-import "./progressBar.css";
+"use client";
 
-function ProgressBar() {
-    return <Progress />
+import React, { useState } from "react";
+import "./progressBar.css";
+//import Progress from "./Progress";
+
+// const message = ['Step 1', 'Step 2', 'Step 3'];
+
+function MultiStep() {
+    return <Multi />
 }
 
-function Progress() {
+function Multi() {
+
+    const [step, setStep] = useState(1);
+    const totalSteps = 3;
+
+    function handlePrev() {
+        if (step > 1) setStep((step)=> step - 1);
+    }
+
+    function handleNext() {
+        if (step < 3) setStep((step) => step + 1);
+    }
+
     return (
         <div className="container">
             <div className="progress_container">
-                <div className="progress"></div>
-                <div className="circle">1</div>
-                <div className="circle">2</div>
-                <div className="circle">3</div>
+                <div className={`${step >= 1 ? "circle active" : "circle"}`}>1</div>
+                <div className={`${step >= 2 ? "circle active" : "circle"}`}>2</div>
+                <div className={`${step >= 3 ? "circle active" : "circle"}`}>3</div>
             </div>
             <div className="btns">
-                <button className="btn active">Prev</button>
-                <button className="btn active">Next</button>
+                <button className="btn" onClick={handlePrev}>Prev</button>
+                <button className="btn" onClick={handleNext}>Next</button>
             </div>
         </div>
     );
 }
-
-export default ProgressBar;
+/*
+function Message({step}) {
+    return <h2>{message[step - 1]}</h2>
+}
+*/
+export default MultiStep;
