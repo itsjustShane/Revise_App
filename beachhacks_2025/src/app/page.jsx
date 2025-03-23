c68f48a08db09c14b7ab85f777391d37aec6b537
adriancancode <vazqueza2004@gmail.com>
2025-03-22 19:17:09 -0700

    Create appointment form

--- beachhacks_2025/src/app/page.jsx
+++ beachhacks_2025/src/app/page.jsx
@@ -1,5 +1,6 @@
 // import { FaBeer } from "react-icons/fa";
-// import Link from "next/link";
+"use client";
+import Link from "next/link";
 import React from "react";
 import Button from "@/components/button/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,9 +10,12 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1 className="text-8xl">Operation Relay</h1>
-        <FontAwesomeIcon icon={faUserNurse} className="w-30 h-30"/>
+        <FontAwesomeIcon icon={faUserNurse} className="w-50 h-30"/>
         <div className="flex flex-row my-10 gap-10">
-        <Button text="Schedule Appointment"></Button>
+          <Link href="/appointment">
+            <Button text="Schedule Appointment" ></Button>
+          </Link>
+        
         <Button text="View Patient Status"></Button>
         </div>
         
--- beachhacks_2025/src/components/button/button.jsx
+++ beachhacks_2025/src/components/button/button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ text }) => {
   return (
     <button
-      className="px-6 py-3 bg-gray-300 text-black text-xl font-semibold rounded-3xl hover:bg-white hover:cursor-pointer 
+      className="px-6 py-3 m-4 bg-gray-300 text-black text-xl font-semibold rounded-3xl hover:bg-white hover:cursor-pointer 
       transition duration-200 active:opacity-75"
     >
       {text}
